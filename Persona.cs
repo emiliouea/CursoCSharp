6c40d14474713f0e89bff59a2eb237c94ded9c72
emiliosdev <sengua2000@outlook.com>
2025-06-07 22:04:37 -0500

    init repo

---
+++ Persona.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoCSharp;
+
+public class Persona
+{
+    private string nombre;
+    private int edad;
+    private string fechaNacimiento;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public int Edad { get => edad; set => edad = value; }
+    public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+
+
+    public void crearPersona(string _nombre, int _edad, string _fechaVencimiento)
+    {
+        nombre = _nombre;
+        edad = _edad;
+        fechaNacimiento= _fechaVencimiento;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using CursoCSharp;
+ 
+Persona persona = new Persona();
+persona.crearPersona("Emilio", 25, "06-06-2000");
+Console.WriteLine($"Nombre: {persona.Nombre}");
+Console.WriteLine($"Edad: {persona.Edad}");
+Console.WriteLine($"Fecha Vencimiento: {persona.FechaNacimiento}");
